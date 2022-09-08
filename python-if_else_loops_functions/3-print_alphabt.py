7f884dea6e25ffa3684aed4ad7abfdf688a796fb
Denisse Landau <denisselandau97@gmail.com>
2022-09-08 12:50:01 -0300

    print without q or c

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,1 +1,9 @@
-dddd
+#!/usr/bin/python3
+char = "{:c}"
+for i in range(97, 123):
+    if i == 101:
+        continue
+    elif i == 113:
+        continue
+    else:
+        print(char.format(i), end="")

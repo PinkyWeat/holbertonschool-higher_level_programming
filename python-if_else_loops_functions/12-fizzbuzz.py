ddd9d31d02e927a7607bc0d1dc475256ae169c69
Denisse Landau <denisselandau97@gmail.com>
2022-09-12 17:28:02 -0300

    fizz buzz

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            print(f"Fizz", end=(" " if i < 101 else ""))
+            print(f"FizzBuzz", end=(" " if i < 101 else ""))
         elif i % 3 == 0:
             print(f"Fizz", end=(" " if i < 101 else ""))
         elif i % 5 == 0:

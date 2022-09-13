30aa1eb9d3ed10f22ef24454f61bb25ea4343ea9
Denisse Landau <denisselandau97@gmail.com>
2022-09-13 17:36:24 -0300

    without modules

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -6,6 +6,6 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx not in range(0, len(my_list)) or my_list is None:
         return my_list
     else:
-        new_list = list(my_list)
+        new_list = my_list[:]
         new_list[idx] = element
         return new_list

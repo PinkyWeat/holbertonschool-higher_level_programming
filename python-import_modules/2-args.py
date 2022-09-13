65bb57eb370692db1577438cc077970ca61ea6dd
Denisse Landau <denisselandau97@gmail.com>
2022-09-12 22:25:13 -0300

    n arg

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print("1 arguments.")
+    elif len(sys.argv) > 2:
+        print(f"{len(sys.argv) - 1} arguments:")
+    e = 1
+    for arg in sys.argv[1:]:
+        print(f"{e}: {arg}")
+        e += 1
+

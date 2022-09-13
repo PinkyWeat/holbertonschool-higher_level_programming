a7e050fa596fb935119a553f720c8c28c61c6f5d
Denisse Landau <denisselandau97@gmail.com>
2022-09-12 22:28:19 -0300

    output fix

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("0 arguments.")
     elif len(sys.argv) == 2:
-        print("1 arguments:")
+        print("1 argument:")
     elif len(sys.argv) > 2:
         print(f"{len(sys.argv) - 1} arguments:")
     e = 1

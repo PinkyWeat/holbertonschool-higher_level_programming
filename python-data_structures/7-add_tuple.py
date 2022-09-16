9d4082739ab602802f43c5c23c375622a6729033
Denisse Landau <denisselandau97@gmail.com>
2022-09-16 17:02:08 -0300

    more outputs

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == 0 or len(tuple_b) == 0:
-        theTuple = (0, 0)
-        return theTuple
-    elif len(tuple_a) == 1 and len(tuple_b) == 2:
-        theTuple = (tuple_a[0] + tuple_b[0], tuple_b[1])
-        return theTuple
-    elif len(tuple_b) == 1 and len(tuple_a) == 2:
-        theTuple = (tuple_a[0] + tuple_b[0], tuple_a[1])
-        return theTuple
-    elif len(tuple_a) == 1 and len(tuple_b) == 1:
-        theTuple = (tuple_a[0] + tuple_b[0])
-    elif len(tuple_a) == 0 and len(tuple_b) == 2:
-        theTuple = tuple_b
-        return theTuple
-    elif len(tuple_b) == 0 and len(tuple_a) == 2:
-        return tuple_a
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 0:
+            tuple_a = 0, 0
+        else:
+            tuple_a = tuple_a[0], 0
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 0:
+            tuple_b = 0, 0
+        else:
+            tuple_b = tuple_b[0], 0
     theTuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return theTuple

75fec5ed78e7d7a9b0ca87858d318a7261926cfb
Denisse Landau <denisselandau97@gmail.com>
2022-09-16 16:46:03 -0300

    try

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -12,7 +12,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif len(tuple_a) == 1 and len(tuple_b) == 1:
         theTuple = (tuple_a[0] + tuple_b[0])
     elif len(tuple_a) == 0 and len(tuple_b) == 2:
-        return tuple_b
+        theTuple = tuple_b
+        return theTuple
     elif len(tuple_b) == 0 and len(tuple_a) == 2:
         return tuple_a
     theTuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])

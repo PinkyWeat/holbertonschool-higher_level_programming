d4e975d7cafdd1ed0baa3c409bbd605d39fd87c5
Denisse Landau <denisselandau97@gmail.com>
2022-09-16 20:48:23 -0300

    fix output

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 def delete_at(my_list=[], idx=0):
     if my_list == 0:
         return my_list
-    if idx < 0 or not range(0, len(my_list)):
+    if idx < 0 or idx not in range(0, len(my_list)):
         return my_list
     else:
         del my_list[idx]

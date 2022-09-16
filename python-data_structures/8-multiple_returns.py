72aa01d3c466b8d8f433c383781d760c53f18143
Denisse Landau <denisselandau97@gmail.com>
2022-09-16 17:33:22 -0300

    changed entire code

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == None or sentence == "":
-        sentence[0] = None
-        return None
-    theTuple = (len(sentence), sentence[0])
-    return theTuple
+    return (len(sentence), sentence[0]) if sentence != "" else (0, None)

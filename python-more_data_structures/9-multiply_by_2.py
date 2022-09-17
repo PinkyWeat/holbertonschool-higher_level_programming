20332dce096b68248124b86a2420c1c43331a04f
Denisse Landau <denisselandau97@gmail.com>
2022-09-17 17:52:56 -0300

    dic *2

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from re import A
+
+
+def multiply_by_2(a_dictionary):
+    new_dictionary = a_dictionary.copy()
+    for pages in new_dictionary.keys():
+        new_dictionary[pages] *= new_dictionary[pages]
+    return new_dictionary

a53c3a3d0fbee0e49b2ec6c3b7f61f838a0eb8d2
Denisse Landau <denisselandau97@gmail.com>
2022-09-17 15:00:57 -0300

    common elems

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from audioop import add
+
+
+def common_elements(set_1, set_2):
+    common = []
+    for elem in set_1:
+        for elem2 in set_2:
+            addE = False
+            for other_elem in common:
+                if other_elem is elem2:
+                    addE = True
+            # compares elems and
+            if elem is elem2 and addE is False:
+                common.append(elem2)
+    return common

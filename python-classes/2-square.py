92de32d6a538be5c5fbd0848651565aa15b0e8d1
Denisse Landau <denisselandau97@gmail.com>
2022-09-19 16:40:26 -0300

    raise errors; \

---
+++ python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Square Class
+Empty class Square + attribute size.
+"""
+
+
+class Square:
+    """A class
+    a square
+"""
+    def __init__(self, size):
+        """__init__
+        The __init__ method inlitialize the value
+        Attributes:
+            size (int): the size.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size

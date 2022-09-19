bb25839ecfea242f238cafd1aacef77f5e8e9372
Denisse Landau <denisselandau97@gmail.com>
2022-09-19 16:30:04 -0300

    square attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Square Class
+Empty class Square + attribute size.
+"""
+
+class Square:
+    """A class
+    Square.size
+"""
+    def __init__(self, size):
+        self.size = size

4e24da070d3df87829ea5afc7471e47c538126c0
Denisse Landau <denisselandau97@gmail.com>
2022-09-19 17:02:25 -0300

    area def

---
+++ python-classes/3-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Square Class
+Empty class Square + attribute size.
+"""
+
+
+class Square:
+    """A class
+    a square
+"""
+    def __init__(self, size=0):
+        """__init__
+        The __init__ method inlitialize the value
+        Attributes:
+            size (int): the size.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+
+    def area(self):
+        """area
+        calculates area of a square
+        """
+        area = self.__size * self.__size
+        return area

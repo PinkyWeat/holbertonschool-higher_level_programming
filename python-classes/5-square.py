e31bdb6cdedb144037c370afaf2103563c1b0a42
Denisse Landau <denisselandau97@gmail.com>
2022-09-19 20:42:44 -0300

    print the square

---
+++ python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Define a Square class"""
+
+from logging.config import valid_ident
+from multiprocessing.sharedctypes import Value
+
+
+class Square:
+    """"a Square"""
+
+    def __init__(self, size=0):
+        """New Square instance
+        size (int): size of new Square"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Get or set size of Square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets size to a value"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """returns the Square area"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """"prints in stdout the square with char #"""
+        if self.__size == 0:
+            print()
+            return None
+        else:
+            print('\n'.join([''.join('#' * self.__size)] * self.__size))

0c36968d6352203b505f94861542880c182f700a
Denisse Landau <denisselandau97@gmail.com>
2022-09-19 16:49:50 -0300

    raise errors; \

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -8,7 +8,7 @@ class Square:
     """A class
     a square
 """
-    def __init__(self, size):
+    def __init__(self, size=0):
         """__init__
         The __init__ method inlitialize the value
         Attributes:
@@ -16,6 +16,6 @@ class Square:
         """
         if type(size) is not int:
             raise TypeError("size must be an integer")
-        if size <= 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size

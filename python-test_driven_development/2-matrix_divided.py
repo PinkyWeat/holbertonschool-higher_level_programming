d570ff668f17f740a4d18da7ba284500125815ce
Denisse Landau <denisselandau97@gmail.com>
2022-09-22 19:08:53 -0300

    fix pep8 - new string format found

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -5,7 +5,7 @@
 def matrix_divided(matrix, div):
     """comments"""
 
-    biggestMatx = [] # new empty list
+    biggestMatx = []  # new empty list
 
     if not isinstance(div, int or float):
         raise TypeError("div must be a number")
@@ -13,13 +13,14 @@ def matrix_divided(matrix, div):
         raise ZeroDivisionError("division by zero")
     lenR = len(matrix[0])
     for elems in matrix:
-        internMatx = [] # new list for each row
+        internMatx = []  # new list for each row
         if lenR != len(elems):
             raise TypeError("Each row of the matrix must have the same size")
         lenR = len(elems)
         for block in elems:
             if type(block) is not int and type(block) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix (list of lists)"
+                                "of integers/floats")
             newBlock = block / div
             internMatx.append(round(newBlock, 2))
         biggestMatx.append(internMatx)

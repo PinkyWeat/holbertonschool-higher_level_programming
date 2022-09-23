1295ec3d3e7b88168add9a9c9b06327ecacf5d55
Denisse Landau <denisselandau97@gmail.com>
2022-09-23 16:49:14 -0300

    fix import

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """prints My name is <first name> <last name>"""
 
-
-import string
-
-
 def say_my_name(first_name, last_name=""):
     """say's the name"""
 
-    if type(first_name) or type(last_name) is not string:
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
     print(f"My name is {first_name} {last_name}")

5a0364a118e9da51a1fd98411da7b784872d180f
Denisse Landau <denisselandau97@gmail.com>
2022-09-24 12:26:24 -0300

    change in init from priv tu pub atrib

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -6,9 +6,8 @@ class Rectangle:
     """A class Rectangle with it's attributes"""
     def __init__(self, width=0, height=0):
         """New Rectangle instance"""
-        self.__width = width
-        self.__height = height
-
+        self.width = width
+        self.height = height
 
     @property
     def width(self):

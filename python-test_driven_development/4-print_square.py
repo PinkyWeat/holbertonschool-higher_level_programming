1b7f4df8bfe2e1b21893f9544c7aae07ea3ac9f5
Denisse Landau <denisselandau97@gmail.com>
2022-09-24 09:28:21 -0300

    task 4

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""function prints a square with #"""
+
+def print_square(size):
+    """border cases first"""
+    if type(size) is not int and (type(size) is float and size < 0):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for rows in range(size):
+        for cols in range(size):
+            print("#", end='' if cols != size - 1 else '\n')

26c2cdd79af46aeb96d324ea8ffe7cdbfe8fa819
Denisse Landau <denisselandau97@gmail.com>
2022-09-24 09:41:11 -0300

    changed num of ifs

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """function prints a square with #"""
 
+
 def print_square(size):
     """border cases first"""
-    if type(size) is not int and (type(size) is float and size < 0):
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if type(size) is not float and size < 0:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")

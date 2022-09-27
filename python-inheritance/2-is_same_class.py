fadfb21a59176741395c517d851934cf6de3f3f7
Denisse Landau <denisselandau97@gmail.com>
2022-09-27 14:33:02 -0300

    pep8

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -3,6 +3,6 @@
 
 
 def is_same_class(obj, a_class):
-    """function returns True if the obj is exactly an instance of spcfied class"""
+    """function returns True if the obj is exactly an instance of class"""
 
     return type(obj) is a_class

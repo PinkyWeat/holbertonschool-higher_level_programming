2272880c0e3bf0d3ba6b3e1a622b3709627594c0
Denisse Landau <denisselandau97@gmail.com>
2022-09-27 14:31:20 -0300

    prints if obj is exactly an instance of spf class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module"""
+
+
+def is_same_class(obj, a_class):
+    """function returns True if the obj is exactly an instance of spcfied class"""
+    return type(obj) is a_class

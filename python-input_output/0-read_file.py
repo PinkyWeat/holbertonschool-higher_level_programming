90062088bb6f887e0d29d3e117142ff7f53cffbc
Denisse Landau <denisselandau97@gmail.com>
2022-09-28 14:37:47 -0300

    .read()

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Module"""
+
+
+def read_file(filename=""):
+    """reads a text file (UT8) and prints it to stdout"""
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
+    print(read_data)

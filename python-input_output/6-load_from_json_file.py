e9d8cc9252e8202ae755b22df81c74f1af1fdeb3
Denisse Landau <denisselandau97@gmail.com>
2022-09-28 18:01:36 -0300

    creates obj from a JSON

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module"""
+import json
+
+
+def load_from_json_file(filename):
+    """creates an Object from a JSON file"""
+    with open(filename, 'w') as tempFile:
+        return json.load(filename)

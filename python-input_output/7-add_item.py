fc17a6c71d911964967c25773b42e1861cbfabc3
Denisse Landau <denisselandau97@gmail.com>
2022-09-28 19:42:49 -0300

    commit please

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,13 +9,13 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if __name__ == '__main__':
-    json_list = []
-    filename = 'add_item.json'
+    theList = []
+    fileInQuestion = 'add_item.json'
     try:
-        json_list = load_from_json_file(filename)
+        theList = load_from_json_file(fileInQuestion)
     except Exception:
         pass
     if len(sys.argv) > 1:
         for arg in sys.argv[1:]:
-            json_list.append(arg)
-    save_to_json_file(json_list, filename)
+            theList.append(arg)
+        save_to_json_file(theList, fileInQuestion)

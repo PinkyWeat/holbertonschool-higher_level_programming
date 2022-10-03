6e6f6221bf2ebb5519964e261efddd1839527e2b
Denisse Landau <denisselandau97@gmail.com>
2022-10-03 20:27:45 -0300

    change in upd\

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -33,14 +33,13 @@ class Square(Rectangle):
 
     def update(self, *args, **kwargs):
         """updates Square data"""
-        length = len(args)
-        if length > 0:
+        if len(args) >= 1:
             self.id = args[0]
-            if length > 2:
+            if len(args) == 2:
                 self.size = args[1]
-            if length > 3:
+            if len(args) == 3:
                 self.x = args[2]
-            if length > 4:
+            if len(args) == 4:
                 self.y = args[3]
         else:
             for key, value in kwargs.items():

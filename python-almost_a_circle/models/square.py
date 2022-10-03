1f8c460b2cb7b3426a3b40e0a59140e1da1e038e
Denisse Landau <denisselandau97@gmail.com>
2022-10-03 19:56:43 -0300

    new class Square

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Python Interpreter"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """new class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """new init Square"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return f"[Square] {self.x}/{self.y} - {self.size}"
+
+    @property
+    def size(self):
+        """New property"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set private size attribute"""
+        self.width = value
+        self.height = value

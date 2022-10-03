16df0096355f82edd544193e0109425f44711459
Denisse Landau <denisselandau97@gmail.com>
2022-10-03 20:32:12 -0300

    changed in update == for >

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -35,11 +35,11 @@ class Square(Rectangle):
         """updates Square data"""
         if len(args) >= 1:
             self.id = args[0]
-            if len(args) == 2:
+            if len(args) > 1:
                 self.size = args[1]
-            if len(args) == 3:
+            if len(args) > 2:
                 self.x = args[2]
-            if len(args) == 4:
+            if len(args) > 3:
                 self.y = args[3]
         else:
             for key, value in kwargs.items():

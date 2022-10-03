e704efc959b6f1bfc72a606e761e1ef26c3c8690
Denisse Landau <denisselandau97@gmail.com>
2022-10-03 20:57:52 -0300

    more info

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -144,4 +144,5 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """returns the dictonary rep"""
-        return self.__dict__
+        return {'id': self.id, 'width': self.width, 'height': self.height,
+         'x': self.x, 'y': self.y}

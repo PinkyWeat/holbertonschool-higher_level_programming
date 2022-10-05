5e021a556b26906c425872359268f0267123a9da
Denisse Landau <denisselandau97@gmail.com>
2022-10-05 14:42:26 -0300

    almost all tests

---
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Python Interpreter"""
+import unittest
+from models.rectangle import Rectangle
+
+class TestRectangle1(unittest.TestCase):
+    """this will test Rectangle's capacity"""
+
+    def test_1(self):
+        """init args(1, 2)"""
+        self.r = Rectangle(1, 2)
+        self.assertEqual(self.r.width, 1)
+        self.assertEqual(self.r.height, 2)
+
+    def test_1b(self):
+        """init args(1, 2, 3)"""
+        self.r = Rectangle(1, 2, 3)
+        self.assertEqual(self.r.width, 1)
+        self.assertEqual(self.r.height, 2)
+        self.assertEqual(self.r.x, 3)
+
+    def test_1c(self):
+        """init args(1, 2, 3, 4)"""
+        self.r = Rectangle(1, 2, 3, 4)
+        self.assertEqual(self.r.width, 1)
+        self.assertEqual(self.r.height, 2)
+        self.assertEqual(self.r.x, 3)
+        self.assertEqual(self.r.y, 4)
+
+    def test_1d(self):
+        """init args("1", 2), etc"""
+        self.assertRaises(TypeError, Rectangle, "1", 2)
+        self.assertRaises(TypeError, Rectangle, 1, "2")
+        self.assertRaises(TypeError, Rectangle, 1, 2, "3")
+        self.assertRaises(TypeError, Rectangle, 1, 2, 3, "4")
+
+    def test_1e(self):
+        """init args (1, 2, 3, 4, 5)"""
+        self.r = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(self.r.width, 1)
+        self.assertEqual(self.r.height, 2)
+        self.assertEqual(self.r.x, 3)
+        self.assertEqual(self.r.y, 4)
+        self.assertEqual(self.r.id, 5)
+
+    def test_1f(self):
+        """init args (-1, 2)"""
+        self.assertRaises(ValueError, Rectangle, -1, 2)
+        self.assertRaises(ValueError, Rectangle, 1, -2)
+        self.assertRaises(ValueError, Rectangle, 0, 2)
+        self.assertRaises(ValueError, Rectangle, 1, 0)
+        self.assertRaises(ValueError, Rectangle, 1, 2, -3)
+        self.assertRaises(ValueError, Rectangle, 1, 2, 3, -4)

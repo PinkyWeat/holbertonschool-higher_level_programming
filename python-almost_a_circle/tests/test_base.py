f7b698bdda69b6a4a63a7fd35cbb9329a1e81138
Denisse Landau <denisselandau97@gmail.com>
2022-10-05 13:57:27 -0300

    first try tests

---
+++ python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Unittest moment"""
+import unittest
+from models.base import Base
+from models.square import Square
+
+class TestBase(unittest.TestCase):
+    """this will test the id attribute"""
+
+    def setUp(self):
+        self.b = Base()
+
+    def test_1(self):
+        self.assertEqual(self.b.id, 1)
+
+    def test_2(self):
+        self.assertEqual(self.b.id, 2)
+
+    def test_3(self):
+        self.assertEqual(self.b.id, 3)
+
+class TestBase420(unittest.TestCase):
+    """test for ID assignment"""
+
+    def test_1(self):
+        self.b = Base(420)
+        self.assertEqual(self.b.id, 420)
+
+class TestToJsonString(unittest.TestCase):
+    """tests Base.to_json_string() method"""
+
+    def setUp(self):
+        self.b = Base()
+
+    def test_1(self):
+        self.assertEqual(self.b.to_json_string(None), "[]")
+
+    def test_1(self):
+        self.assertEqual(self.b.to_json_string([1, 2]), "[1, 2]")

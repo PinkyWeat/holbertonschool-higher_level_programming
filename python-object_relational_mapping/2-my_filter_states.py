c1bc1010e7084e249de50281246252259ffcd742
Denisse Landau <denisselandau97@gmail.com>
2022-11-11 17:46:42 +0000

    commit

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """Python Module"""
-import MySQLdb
+import MySQLdataB
 from sys import argv
 
 
 """SQL Query"""
-if __name__ = "__main__"
+if __name__ == "__main__":
     dataB = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=argv[1],
         password=argv[2],
         database=argv[3]
-    )
+        )
+
     cursor = dataB.cursor()
     cursor.execute('''SELECT * FROM states WHERE name LIKE "{}"
     ORDER BY states.id ASC;'''.format(argv[4]))

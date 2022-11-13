d49cb60ab6415db9273d09a63b0e2fb6b635cf1c
Denisse Landau <denisselandau97@gmail.com>
2022-11-13 17:50:47 -0300

    8-model_state_fetch_first.py

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     try:
         engine = create_engine(
             'mysql+mysqldb://{}:{}@localhost:3306/{}'
-            .format(argv[1], argv[2], argv[3]), pool_pre_ping=True
+            .format(argv[1], argv[2], argv[3]),
         )
     except Exception:
         print("Error")

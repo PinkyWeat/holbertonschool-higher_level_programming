14ab1822c61e2d5d297529950e7d61d1121ab6aa
Denisse Landau <denisselandau97@gmail.com>
2022-11-13 15:55:31 -0300

    class state

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module State"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+class State(Base):
+    """New class State"""
+
+    __tablename__ = 'states'
+    id = Column('id', Integer, primary_key=True,
+                autoincrement=True, unique=True, nullable=False)
+    name = Coumn('name', String(128), nullable=False)

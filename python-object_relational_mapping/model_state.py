cc28c875c745fb3bce3e23ecfb2fe9b71a617684
Denisse Landau <denisselandau97@gmail.com>
2022-11-13 15:59:37 -0300

    class state

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Module State
-"""
+"""State model"""
+
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -9,9 +10,9 @@ Base = declarative_base()
 
 
 class State(Base):
-    """New class: State"""
+    """class: State"""
 
     __tablename__ = 'states'
     id = Column('id', Integer, primary_key=True,
                 autoincrement=True, unique=True, nullable=False)
-    name = Coumn('name', String(128), nullable=False)
+    name = Column('name', String(128), nullable=False)

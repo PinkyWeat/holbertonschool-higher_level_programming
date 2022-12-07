4832f781ed76c80d458c0be1af4e1ba3486a104b
Denisse Landau <denisselandau97@gmail.com>
2022-12-07 17:07:43 -0300

    js is amazing

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const cubes = []; let counter = 0;
+cubes.push('C is fun', 'Python is cool', 'JavaScript is amazing');
+
+while (counter < cubes.length) {
+  console.log(cubes[counter]);
+  counter++;
+}

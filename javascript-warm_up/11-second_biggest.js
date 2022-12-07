214b44ea58057c296f39e315f80dc28cf57ae601
Denisse Landau <denisselandau97@gmail.com>
2022-12-07 18:02:36 -0300

    yas kween

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -2,10 +2,16 @@
 let len = process.argv.length;
 let sorted = [];
 
-while (len > 2) {
-  len--; sorted.push(process.argv[len]);
-} // copy paste of given nums
+if (process.argv[3]) {
+  while (len > 2) {
+    len--; sorted.push(process.argv[len]);
+  } // copy paste of given nums
 
-sorted = sorted.sort((a, b) => a - b); // sorting for easier comparison
-const res = sorted[sorted.length - 2]; // process.argv.length - 4 works
-console.log(res);
+  sorted = sorted.sort((a, b) => a - b); // sorting for easier comparison
+  const res = sorted[sorted.length - 2]; // process.argv.length - 4 works
+  console.log(res);
+} else if (process.argv[2]) {
+  console.log(process.argv[2]);
+} else {
+  console.log(0);
+}

d9a173cb02bbeb10d90b92b3c2dc6d8087491a7b
Denisse Landau <denisselandau97@gmail.com>
2022-12-09 19:39:36 -0300

    task 6

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -7,7 +7,7 @@ module.exports = class Rectangle {
     }
   }
 
-  print (c = 'X') {
+  print (c) {
     // first will concat width
     let guith = '';
     for (let uith = 0; uith < this.width; uith++) {

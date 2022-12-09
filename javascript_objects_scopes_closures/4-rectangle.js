ecfc3eb57bb4c07c780647398cdc3576d016e6c0
Denisse Landau <denisselandau97@gmail.com>
2022-12-09 17:13:33 -0300

    more methods xd

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    // first will concat width
+    let guith = '';
+    for (let uith = 0; uith < this.width; uith++) {
+      guith += 'X';
+    }
+    // then print the width * height times
+    for (let jait = 0; jait < this.height; jait++) {
+      console.log(guith);
+    }
+  }
+
+  rotate () {
+    const helper = this.width;
+    this.width = this.height;
+    this.height = helper;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+};

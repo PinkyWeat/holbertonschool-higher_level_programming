469fb34e5ba4b8348f6eb1e038124a429673d4c3
Denisse Landau <denisselandau97@gmail.com>
2022-12-12 15:50:49 -0300

    checker check

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -4,11 +4,11 @@ const request = require('request');
 const id = process.argv[2];
 
 request('https://swapi-api.hbtn.io/api/films/' + id, function (err, response, body) {
-    if (err) {
-        return console.log(err);
-    } else {
-        if (response.statusCode === 200) {
-        console.log(JSON.parse(body).title);
-        }
+  if (err) {
+    return console.log(err);
+  } else {
+    if (response.statusCode === 200) {
+      console.log(JSON.parse(body).title);
     }
+  }
 });

23d5b647b35e3bc7d5b75e5c73ad2a4cd481d5f4
Denisse Landau <denisselandau97@gmail.com>
2022-12-12 15:21:42 -0300

    get status code

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const https = require('https');
-const url = process.argv[2]
+const url = process.argv[2];
 
 https.get(url, (response) => {
-    console.log(`code: ${response.statusCode}`);
+  console.log('code: ', response.statusCode);
 });

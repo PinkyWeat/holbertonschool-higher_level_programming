c8c732b0780fcbeeb1eda869f8f19ab0dad2f6a9
Denisse Landau <denisselandau97@gmail.com>
2022-12-12 14:27:42 -0300

    read and print content of file

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs'); // NODE.js built-in module
-let args = process.argv;
+const args = process.argv;
 
-fs.readFile(args[2], 'utf8', (err, data)=>{
-    if(err) {
-        return console.log(err);
-    }
-    console.log(data);
+fs.readFile(args[2], 'utf8', (err, data) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
 });

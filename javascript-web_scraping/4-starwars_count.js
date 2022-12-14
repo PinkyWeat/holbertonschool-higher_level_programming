1e17f3d1854d762c45db603d70a82ceaf98e9838
Denisse <landau@MacBook-Air-de-Denis.local>
2022-12-14 16:28:47 -0300

    character num of times

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -5,13 +5,17 @@ const id = 18;
 
 request('https://swapi-api.hbtn.io/api/films/' + id, function (err, response, body) {
   if (err) {
-    return console.log(err);
+    return console.log(err + ' ' + response.statusCode);
   } else {
     if (response.statusCode === 200) {
-      const data = JSON.parse(body);
+      const data = JSON.parse(body).results;
       let count = 0;
-      for (data.title of data) {
-        count += 1;
+      for (const each of data) {
+        for (const chars of each.characters) {
+          if (chars.endsWith('/18/')) {
+            ++count;
+          }
+        }
       }
       console.log(count);
       return count;
